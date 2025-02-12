ee1ba64d82682f0a5aca0c669a02653cf097d8d6
poncardasm <159589562+poncardasm@users.noreply.github.com>
2025-02-12 18:50:00 +0200

    Remove .nvmrc file and update metadata.js with corrected URLs and improved description

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,17 +1,17 @@
 export default {
 	title: "Mchael Poncardas",
-	url: "https://www.poncardas.com/", // Updated to match the author's URL
+	url: "https://www.poncardas.com/",
 	language: "en",
-	description: "I'm Mac! Welcome to my corner of the internet.",
+	description: "I'm Mac! Welcome to my little corner of the internet.",
 	author: {
 		name: "Mchael Poncardas",
 		email: "poncardasm@fastmail.com",
-		url: "https://www.poncardas.com/about-me/", // Updated to use the correct URL
+		url: "https://www.poncardas.com/about-me/",
 	},
 	social: {
-		twitter: "https://twitter.com/poncardasm", // Added social media link
-		github: "https://github.com/poncardasm", // Added GitHub profile link
-		linkedin: "https://www.linkedin.com/in/poncardas", // Added LinkedIn profile link
+		twitter: "https://twitter.com/poncardasm",
+		github: "https://github.com/poncardasm",
+		linkedin: "https://www.linkedin.com/in/poncardas",
 	},
 	currentYear: new Date().getFullYear(),
 };
